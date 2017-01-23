1734f233ce27db9efa390c1b8dbeb8fe88da7455
IlanZerdoun <ilanzerdoun@MacBook-Pro-de-Ilan.local>
2017-01-23 15:03:27 +0100

    First page, CSS, HTML

---
+++ JS/Soze.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('#instagram').hover(function(){ 
+        $('#instagram').attr('src','Images/Icones/Instagram.png');
+    },
+    function(){ $('#instagram').attr('src','Images/Icones/Instagram42.png')
+    });
+});
+
+$(document).ready(function(){
+    $('#twitter').hover(function(){                       $('#twitter').attr('src','Images/Icones/Twitter.png');
+    },
+    function(){
+    $('#twitter').attr('src','Images/Icones/twitter42.png')
+    });
+});
+
+$(document).ready(function(){
+    $('#facebook').hover(function(){                       $('#facebook').attr('src','Images/Icones/Facebook.png');
+    },
+    function(){
+    $('#facebook').attr('src','Images/Icones/facebook42.png')
+    });
+});
+
+$(document).ready(function(){
+    $('#google_plus').hover(function(){                     $('#google_plus').attr('src','Images/Icones/Google+.png');
+    },
+    function(){
+    $('#google_plus').attr('src','Images/Icones/google-plus42.png')
+    });
+});
